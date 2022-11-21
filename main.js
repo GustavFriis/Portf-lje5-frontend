bf080062b8778b1162eadefaa0c71614c5e87244
Oskar <lansoskar@gmail.com>
2022-11-21 10:37:24 +0100

    basic

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("test")
+fetch('https://cafe-api-jcky.onrender.com/cafes')
+    .then(response => response.json())
+    .then(function (cafes) {
+       renderCafes(cafes)
+        console.log(cafes)
+    });
+
+function renderCafes(cafes) {
+    const ul = document.querySelector('ul.cafes')
+    cafes.forEach(cafes => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <p>${cafes.CafeName}</p>
+        <p>${cafes.Cozy}</p>
+        <p>${cafes.PriceRange}</p>
+        <p>${cafes.Wifi}</p>
+        <p>${cafes.Postcode}</p>
+        <p>${cafes.Address}</p>
+        <p>${cafes.Size}</p>
+        `;
+        ul.appendChild(li);
+    });
+}

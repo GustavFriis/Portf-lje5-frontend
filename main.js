e947d6d9b08cef93ef082c317469e4a70033e6ca
madsen10 <91535029+madsen10@users.noreply.github.com>
2022-11-24 10:43:58 +0100

    searchbar

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ fetch('https://cafe-api-jcky.onrender.com/cafes')
        renderCafes(cafes)
         console.log(cafes)
         cafesGlobal = cafes
+        filterList(cafes)
     });
 
 function renderCafes(cafes) {
@@ -27,6 +28,7 @@ function renderCafes(cafes) {
 }
 
 function getCozyValue(cafes){
+
     const selectedCozyValue = document.getElementById("cozy").value;
     console.log(selectedCozyValue)
     // vi skal have ændred værdierne i render til at sige cozyLow, cozyMedium og cozyHigh, og lavet loW omtil low i render
@@ -37,6 +39,7 @@ function getCozyValue(cafes){
 
 
 function getPriceValue(cafes){
+    debugger;
     const selectedPriceValue = document.getElementById("price").value;
     console.log(selectedPriceValue)
     // vi skal have ændred værdierne i render til at sige priceLow, priceMedium og priceHigh
@@ -44,3 +47,28 @@ function getPriceValue(cafes){
     console.log(filteredPriceFilter)
     renderCafes(filteredPriceFilter)
 }
+
+
+
+function filterList (cafes) {
+    console.log(cafes)
+    const textInput = document.querySelector('input#searchBarID');
+    textInput.addEventListener("input", function () {
+        console.log(textInput)
+        const inputValue = textInput.value
+        console.log(inputValue)
+        const filteredCafes = [];
+        for (let i = 0; i < cafesGlobal.length; i++) {
+            const newCafe = cafesGlobal[i]
+            console.log(newCafe.CafeName)
+            if (newCafe.CafeName.toLowerCase().includes(inputValue)){
+                filteredCafes.push(newCafe)
+            }
+        }
+        renderCafes(filteredCafes)
+        console.log(filteredCafes)
+    })
+}
+
+
+
